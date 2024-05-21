edf2a38a2f8e080353516e4eff5d4f1d930b9c9c
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-05-21 16:54:59 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import UserCart from './UserCart';
 
 function App() {
+
+  // Example2
+
+  const userData = [
+    { name: "Lokesh", age: 24, email: "lucky9644@gmail.com" },
+    { name: "Asyush", age: 25, email: "aayush9644@gmail.com" },
+    { name: "Manish", age: 24, email: "manish9644@gmail.com" },
+    { name: "Chandu", age: 24, email: "chandu9644@gmail.com" }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello in React js</h1>
+      <UserCart name="Nilesh Choudhary" />
+
+
+      {userData.map((user, index) => (
+        <UserCart
+          key={index}
+          name={user.name}
+          age={user.age}
+          email={user.email}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/UserCart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const UserCart = (props) => {
+    console.log(props)
+
+
+    return (
+        <div>
+            <h1>Hello {props.name}</h1>
+
+            {/* <h2>name: {name}</h2>
+            <p>age: {age}</p>
+            <p>Email: {email}</p> */}
+
+        </div>
+    );
+}
+
+export default UserCart;
