37b796d4a877ab24b94aa8212e839eaa49091ed3
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-05-21 17:03:48 +0530

    update props data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import UserCart from './UserCart';
+import { useState } from 'react';
 
 function App() {
 
+  const [name, setName] = useState("Nilesh Choudhary");
+
   // Example2
 
-  const userData = [
-    { name: "Lokesh", age: 24, email: "lucky9644@gmail.com" },
-    { name: "Asyush", age: 25, email: "aayush9644@gmail.com" },
-    { name: "Manish", age: 24, email: "manish9644@gmail.com" },
-    { name: "Chandu", age: 24, email: "chandu9644@gmail.com" }
-  ]
+  // const userData = [
+  //   { name: "Lokesh", age: 24, email: "lucky9644@gmail.com" },
+  //   { name: "Asyush", age: 25, email: "aayush9644@gmail.com" },
+  //   { name: "Manish", age: 24, email: "manish9644@gmail.com" },
+  //   { name: "Chandu", age: 24, email: "chandu9644@gmail.com" }
+  // ]
   return (
     <div className='App'>
       <h1>Hello in React js</h1>
-      {/* <UserCart name="Nilesh Choudhary" /> */}
-
+      <UserCart name={name} />
+      {/* 
       {userData.map((user, index) => (
         <UserCart
           key={index}
@@ -24,7 +27,9 @@ function App() {
           age={user.age}
           email={user.email}
         />
-      ))}
+      ))} */}
+
+      <button onClick={() => setName("chanduBhai")}>Update Props</button>
     </div>
   );
 }
