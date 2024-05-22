a99724326d12ef73c9526f6878fb1632e8cb9fc7
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-05-22 08:10:55 +0530

    target Input values

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import UserCart from './UserCart';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [isValue, setIsValue] = useState("");
+  const [inputValueShow, setInputValueHide] = useState(false);
 
-  const [name, setName] = useState("Nilesh Choudhary");
 
-  // Example2
+  const handleClick = (evennt) => {
+    console.log(evennt.target.value)
+    setIsValue(evennt.target.value);
 
-  // const userData = [
-  //   { name: "Lokesh", age: 24, email: "lucky9644@gmail.com" },
-  //   { name: "Asyush", age: 25, email: "aayush9644@gmail.com" },
-  //   { name: "Manish", age: 24, email: "manish9644@gmail.com" },
-  //   { name: "Chandu", age: 24, email: "chandu9644@gmail.com" }
-  // ]
+    setInputValueHide(false);
+  }
   return (
     <div className='App'>
-      <h1>Hello in React js</h1>
-      <UserCart name={name} />
-      {/* 
-      {userData.map((user, index) => (
-        <UserCart
-          key={index}
-          name={user.name}
-          age={user.age}
-          email={user.email}
-        />
-      ))} */}
+      <h1>{inputValueShow ? isValue : ""}</h1>
+      <input type="text" onChange={handleClick} />
 
-      <button onClick={() => setName("chanduBhai")}>Update Props</button>
+      <button onClick={() => setInputValueHide(true)}>submit</button>
     </div>
   );
 }
--- src/UserCart.js
+++ src/UserCart.js
@@ -1,19 +1,11 @@
 import React from 'react';
 
-const UserCart = (props) => {
-    console.log(props)
-
-    // const { name, age, email } = props;
-
-
-    return (
-        <div>
-            <h1>Hello {props.name}</h1>
-            {/* <h2>name: {name}</h2>
-            <p>age: {age}</p>
-            <p>Email: {email}</p> */}
-        </div>
-    );
+const UserCart = () => {
+  return (
+    <div>
+      
+    </div>
+  );
 }
 
 export default UserCart;
